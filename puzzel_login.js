098e57f984f75ed42ada39fc1ba6f385a245fdec
alexander-mdq <107989723+alexander-mdq@users.noreply.github.com>
2022-06-22 10:40:40 +0200

    Create puzzel_login.js

---
+++ puzzel_login.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Puzzel login test
+// @namespace https://admin.puzzel.com
+// @version 0.0.1
+// @description Puzzel admin page
+// @match https://admin.puzzel.com
+// @updateURL 
+// ==/UserScript==
